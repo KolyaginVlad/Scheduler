26cc96c0d68e350ef94d9edc795d0de1ac490b67
KolyaginVlad <50174448+KolyaginVlad@users.noreply.github.com>
2021-02-06 17:29:42 +0300

    create Task

---
+++ app/src/main/java/ru/same/scheduler/Task.java
@@ -0,0 +1,17 @@
+package ru.same.scheduler;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class Task extends View {
+    public Task(Context context) {
+        super(context);
+    }
+
+    public Task(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
