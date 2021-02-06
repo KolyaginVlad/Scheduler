042f6d132423ed567773f0732464d57d92c35a59
KolyaginVlad <50174448+KolyaginVlad@users.noreply.github.com>
2021-02-06 18:07:04 +0300

    create task tree

--- app/src/main/java/ru/same/scheduler/FirstFragment.java
+++ app/src/main/java/ru/same/scheduler/FirstFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -22,13 +23,12 @@ public class FirstFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        LinearLayout layout = (LinearLayout) view.findViewById(R.id.layout1);
 
-        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
-            }
-        });
+        for (int i = 0; i <10 ; i++) {
+            Task task = new Task(view.getContext());
+            task.setAllInfo("Title"+ i, "Body" + i,""+i);
+            layout.addView(task);
+        }
     }
 }
--- app/src/main/java/ru/same/scheduler/Task.java
+++ app/src/main/java/ru/same/scheduler/Task.java
@@ -1,17 +1,42 @@
 package ru.same.scheduler;
 
 import android.content.Context;
-import android.util.AttributeSet;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 
-public class Task extends View {
+public class Task extends LinearLayout {
+    private TextView title;
+    private TextView body;
+    private TextView time;
+
     public Task(Context context) {
         super(context);
+        init(context);
     }
 
-    public Task(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
+    private void init(Context context){
+        View.inflate(context,R.layout.task,this);
+        title = findViewById(R.id.taskTitle);
+        body = findViewById(R.id.taskBody);
+        time = findViewById(R.id.taskTime);
+    }
+
+    public void setTitle(String title) {
+        this.title.setText(title);
+    }
+
+    public void setBody(String body) {
+        this.body.setText(body);
+    }
+
+    public void setTime(String time) {
+        this.time.setText(time);
+    }
+    public void setAllInfo(String tit, String bod, String tim){
+        title.setText(tit);
+        body.setText(bod);
+        time.setText(tim);
     }
 }
