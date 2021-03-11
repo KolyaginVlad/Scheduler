4ffe948621baca6c3c455d4bf690dfbce37ac1c7
KolyaginVlad <50174448+KolyaginVlad@users.noreply.github.com>
2021-03-11 13:43:29 +0300

    fix getPathFromUri

--- app/src/main/java/ru/same/scheduler/TaskRewrite.java
+++ app/src/main/java/ru/same/scheduler/TaskRewrite.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.DocumentsContract;
 import android.provider.MediaStore;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
@@ -95,19 +96,6 @@ public class TaskRewrite extends Fragment {
 
     }
 
-    //    public String getRealPathFromURI(Uri uri) {
-//        String path = "";
-//        if (getApplicationContext().getContentResolver() != null) {
-//            Cursor cursor = getApplicationContext().getContentResolver().query(uri, null, null, null, null);
-//            if (cursor != null) {
-//                cursor.moveToFirst();
-//                int idx = cursor.getColumnIndex(MediaStore.Files.FileColumns.DATA);
-//                path = cursor.getString(idx);
-//                cursor.close();
-//            }
-//        }
-//        return path;
-//    }
     public static String getPathFromUri(final Context context, final Uri uri) {
 
 
@@ -128,11 +116,66 @@ public class TaskRewrite extends Fragment {
             // DownloadsProvider
             else if (isDownloadsDocument(uri)) {
 
-                final String id = DocumentsContract.getDocumentId(uri);
-                final Uri contentUri = ContentUris.withAppendedId(
-                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    final String id;
+                    Cursor cursor = null;
+                    try {
+                        cursor = context.getContentResolver().query(uri, new String[]{MediaStore.MediaColumns.DISPLAY_NAME}, null, null, null);
+                        if (cursor != null && cursor.moveToFirst()) {
+                            String fileName = cursor.getString(0);
+                            String path = Environment.getExternalStorageDirectory().toString() + "/Download/" + fileName;
+                            if (!TextUtils.isEmpty(path)) {
+                                return path;
+                            }
+                        }
+                    } finally {
+                        if (cursor != null)
+                            cursor.close();
+                    }
+                    id = DocumentsContract.getDocumentId(uri);
+                    if (!TextUtils.isEmpty(id)) {
+                        if (id.startsWith("raw:")) {
+                            return id.replaceFirst("raw:", "");
+                        }
+                        String[] contentUriPrefixesToTry = new String[]{
+                                "content://downloads/public_downloads",
+                                "content://downloads/my_downloads"
+                        };
+                        for (String contentUriPrefix : contentUriPrefixesToTry) {
+                            try {
+                                final Uri contentUri = ContentUris.withAppendedId(Uri.parse(contentUriPrefix), Long.valueOf(id));
 
-                return getDataColumn(context, contentUri, null, null);
+                         /*   final Uri contentUri = ContentUris.withAppendedId(
+                                    Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));*/
+
+                                return getDataColumn(context, contentUri, null, null);
+                            } catch (NumberFormatException e) {
+                                //In Android 8 and Android P the id is not a number
+                                return uri.getPath().replaceFirst("^/document/raw:", "").replaceFirst("^raw:", "");
+                            }
+                        }
+
+
+                    }
+
+                } else {
+                    Uri contentUri = Uri.parse("");
+                    final String id = DocumentsContract.getDocumentId(uri);
+                    final boolean isOreo = Build.VERSION.SDK_INT >= Build.VERSION_CODES.O;
+                    if (id.startsWith("raw:")) {
+                        return id.replaceFirst("raw:", "");
+                    }
+                    try {
+                        contentUri = ContentUris.withAppendedId(
+                                Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+
+                    } catch (NumberFormatException e) {
+                        e.printStackTrace();
+                    }
+                    if (contentUri != null) {
+                        return getDataColumn(context, contentUri, null, null);
+                    }
+                }
             }
             // MediaProvider
             else if (isMediaDocument(uri)) {
